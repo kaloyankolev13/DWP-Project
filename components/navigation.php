ab3e3a17eb7effaedbe9e47320a1ca714bbb7ab6
kaloyankolev13 <63497199+kaloyankolev13@users.noreply.github.com>
2023-12-09 04:19:49 +0100

    adding errors

--- components/navigation.php
+++ components/navigation.php
@@ -1,4 +1,8 @@
 <?php
+require_once 'controllers/User.php'; // Adjust the path as necessary
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
 $menuItems = [
     ['url' => '/', 'label' => 'Home'],
--- router.php
+++ router.php
@@ -1,5 +1,9 @@
 <?php
 require_once 'controllers/User.php'; 
+require_once 'controllers/User.php'; // Adjust the path as necessary
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
     $userObj = new User(); // Instantiate the User object
 
